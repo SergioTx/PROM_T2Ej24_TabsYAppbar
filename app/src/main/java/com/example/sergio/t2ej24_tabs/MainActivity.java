4d620551f20a041a47933fc6192a7db843d60043
SergioTx <txitxo37@gmail.com>
2016-10-25 10:12:31 +0200

    Initial commit NO FUNCIONA

---
+++ app/src/main/java/com/example/sergio/t2ej24_tabs/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.sergio.t2ej24_tabs;
+
+import android.content.res.Resources;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TabHost;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Resources res = getResources();
+
+        TabHost tabs = (TabHost) findViewById(android.R.id.tabhost);
+        tabs.setup();
+
+        TabHost.TabSpec spec = tabs.newTabSpec("tab1");
+        spec.setContent(R.id.tab1);
+        spec.setIndicator(getString(R.string.llamadas));
+        tabs.addTab(spec);
+
+        spec = tabs.newTabSpec("tab2");
+        spec.setContent(R.id.tab2);
+        spec.setIndicator(getString(R.string.chats));
+        tabs.addTab(spec);
+
+        spec = tabs.newTabSpec("tab3");
+        spec.setContent(R.id.tab3);
+        spec.setIndicator(getString(R.string.contactos));
+        tabs.addTab(spec);
+
+        tabs.setCurrentTab(0);
+
+        tabs.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
+            public void onTabChanged(String s) {
+                Log.i("AndroidTabsDemo", "Pulsada pestaña: " + s);
+            }
+        });
+    }
+}
